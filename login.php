586cc17f091e89e958c0bb884f87b6610283f7e8
devromolo <90993991+devromolo@users.noreply.github.com>
2021-11-22 17:24:44 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,10 +4,10 @@
 include('conexao.php');
 
 //Parametros para pegar o login/senha
-$login = $_POST['email'];
+$login = $_POST['login'];
 $senha = $_POST['senha'];
 
-$query = "SELECT email,senha,nome FROM usuario WHERE email = '$login' AND senha = '$senha'";
+$query = "SELECT login,senha,nome FROM usuario WHERE login = '$login' AND senha = '$senha'";
 
 $resultado = mysqli_query($conexao, $query);
 
