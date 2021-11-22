bd7713e7c75b9d5b84b6dc360596bda98e543c57
devromolo <90993991+devromolo@users.noreply.github.com>
2021-11-22 16:56:47 -0300

    update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,11 +3,11 @@
 $hostname = "localhost";
 $user = "root";
 $password = "";
-$database= "estacionamento";
+$database= "bdcadastro";
 $conexao = mysqli_connect($hostname,$user,$password,$database);
 
 if(!$conexao){
-	print "Falha na conexao com o banco de dados";
+	print "falha na conexao com o banco de dados";
 }
 
 ?>
