fd68cd4876a4856ef727e4d189c928435236e634
Eduardo <57955350+EduardoHa733@users.noreply.github.com>
2021-11-26 20:46:24 -0300

    Update bdcadastro.sql

--- bdcadastro.sql
+++ bdcadastro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Nov-2021 às 00:35
+-- Tempo de geração: 27-Nov-2021 às 00:45
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.13
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `estacionamento`
+--
+
+CREATE TABLE `estacionamento` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `reservada` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `estacionamento`
+--
+
+INSERT INTO `estacionamento` (`id`, `descricao`, `reservada`) VALUES
+(1, 'Vaga 01', 0),
+(2, 'Vaga 02', 0),
+(3, 'Vaga 03', 0),
+(4, 'Vaga 04', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `criado` date NOT NULL DEFAULT current_timestamp(),
+  `res_vaga` tinyint(1) NOT NULL,
+  `id_vaga` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `criado`, `res_vaga`, `id_vaga`) VALUES
+(1, 'Eduardo', 'eduardo@eduardo.com', '123123', '2021-11-26', 0, 0),
+(2, 'Rebeka', 'rebeka@rebeka.com', '123123', '2021-11-26', 0, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `vaga`
 --
 
@@ -38,6 +84,12 @@ CREATE TABLE `vaga` (
 --
 
 --
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `vaga`
 --
 ALTER TABLE `vaga`
@@ -46,6 +98,12 @@ ALTER TABLE `vaga`
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `vaga`
